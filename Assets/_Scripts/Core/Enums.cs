709b4c970c69ce409a8121e6bacb69fef721251f
onikatashi <llho717@naver.com>
2026-03-04 03:09:40 +0900

    유니티 폴더 생성 및 일부 파일 생성
    - 씬, 스크립트, 프리팹, 사운드, 데이터 폴더 생성
    - 씬 생성
    - enum 모아두는 스크립트 생성
    - Scene 이름 모아두는 스크립트 생성

---
+++ Assets/_Scripts/Core/Enums.cs
@@ -0,0 +1,4 @@
+public enum AgeState { Child, Youth, Elder }
+public enum TimeMode { None, Accelerate, Revert }
+public enum TimeState { None, Past, Future }
+public enum SkyState { Day, Night }
---
+++ Assets/_Scripts/Core/SceneNames.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public static class SceneNames
+{
+    public const string BOOT = "00_Boot";
+    public const string TITLE = "01_Title";
+    public const string INTRO = "02_Intro";
+    public const string STAGE01 = "03_Stage_01";
+}
