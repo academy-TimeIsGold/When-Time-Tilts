e248c5506bb6ef147839ab25309fc21ecfbf6b17
onikatashi <llho717@naver.com>
2026-03-13 17:13:31 +0900

    skyController 수정

--- Assets/_Scripts/Core/Enums.cs
+++ Assets/_Scripts/Core/Enums.cs
@@ -1,4 +1,4 @@
 public enum AgeState { Child, Youth, Elder }
 public enum TimeMode { None, Accelerate, Revert }
 public enum TimeState { None, Past, Future }
-public enum SkyState { Day, Night }
+public enum SkyState { None, Day, Night }
